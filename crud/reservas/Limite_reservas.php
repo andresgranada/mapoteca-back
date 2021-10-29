3ab302dcbfc2a7ab170d271943ee02ecda1ae327
MiguelMtzV <miguel49825@gmail.com>
2021-10-29 16:12:35 -0500

    cambios

--- crud/reservas/Limite_reservas.php
+++ crud/reservas/Limite_reservas.php
@@ -1,10 +1,10 @@
 <?php
 include_once "../../cors.php";
-if (!isset($_GET["id"])) {
+if (!isset($_GET["Clave_Usuario"])) {
     echo json_encode(null);
     exit;
 }
-$Reserva = $_GET["id"];
+$Reserva = $_GET["Clave_Usuario"];
 include_once "../../funciones.php";
 $Limite = 'Hola';
 echo json_encode($Limite);
